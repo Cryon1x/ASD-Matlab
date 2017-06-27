79b71f4574e8cd1cafce3b796c08399e080f7bba
Vikrant Sharma <sindpower@hotmail.com>
2017-06-27 14:17:56 -0400

    modified files, enhanced processor'

---
+++ ascropper.m
@@ -0,0 +1,63 @@
+appfiles1 = who(); appfiles = cell(0);
+for i = 1:length(appfiles1) %returns ASC files as appfiles 
+   x = strncmpi(appfiles1{i},'ASC',3);
+   if x ~= 1
+       appfiles{i} = '';
+   else
+       appfiles{end+1} = appfiles1{i}; %#ok<SAGROW>
+   end
+end
+for i = 1:length(appfiles)
+    x = strncmpi(appfiles{i},'',1);
+    if x == 1
+        appfiles = appfiles(1:i-1);
+        break
+    end
+end
+clear appfiles1;
+for i=1:length(appfiles) 
+    file = strcat(appfiles{i}, '.textdata');
+    evalc(['ref=' file]);
+    fprintf('File Successfully Imported (%d)\n', i);
+    ref = ref(93:end,1:end);
+    [row,~] = size(ref);
+    j = 1; col = 0; m = 0;
+    while col == 0 % find the first mention of MSG
+        j = j + 1;
+        k = char(ref{j,1});
+        col = strncmpi(k,'MSG',3);
+        if col == 1
+            m = m + 1;
+            col = 0;
+            if m == 3
+                break
+            end
+        end
+    end
+
+    ref1 = ref(1:j-1,1:end);
+    ref = ref(j:end,1:end);
+    filename = 'taringSCL_GSR'; 
+    l = 'ASC'; k = num2str(i);
+    f = strcat(l,k);
+    evalc([f '= setfield(' f ',filename,ref1)']);  % Save tare file to struct
+    for u=1:2
+    filename = ref{1,6};% rename filename
+    filename = strsplit(filename, '\hab_video'); filename = filename{2}; filename = strsplit(filename, '.'); filename = filename{1}; filename = strcat('GSRVideo',filename);
+    while col == 0 % find the next mention of MSG
+        j = j + 1;
+        k = char(ref{j,1});
+        col = strncmpi(k,'MSG',3);
+        [row,~] = size(ref);
+        if j == row
+            break
+        end
+    end
+    ref1 = ref(1:j-1,1:end);
+    ref = ref(j:end,1:end);
+    j = 1; col = 0;
+    evalc([f '= setfield(' f ',filename,ref1)']);
+    end
+    fprintf('File Successfully Processed (%d)\n', i)
+end
+    
--- processor.m
+++ processor.m
@@ -1,7 +1,7 @@
-e = 0;
+ e = 0;
 while e == 0
     clear choice;
-choice = menu('Choose a Function','Import Data','Convert .bdt files to .mat','Crop data', 'Clear Workspace','Unclutter Workspace','Update Workspace');
+choice = menu('Choose a Function','Import Data','Convert .bdt files to .mat','Crop data', 'Clear Workspace','Unclutter Workspace','GSR Cropper','Update Workspace');
 
 switch choice 
     case 1
@@ -351,24 +351,103 @@ mkdir('Cropped_Data');
    
  
     case 4
-        clear all;
-        e = 0;
-    case 5
-        x = who();
-identifier2 = {'.csv','.asc', 'fold_list'};
-for i=1:length(x)
-    for j=1:length(identifier2)
-        str1 = char(x{i});
-        str2 = char(identifier2{j});
-        y = strcmp(str1, str2);
-        if y ~= 1
-        clear x(i);
+    x = who();
+    y = 0;
+        identifier2 = {'x','i','j','identifier2','y','str1','str2'};
+        for i=1:length(x)
+            for j=1:length(identifier2)
+                str1 = char(x{i});
+                str2 = char(identifier2{j});
+                y = y + strncmpi(str1, str2,3);
+            end
+            if y == 0
+                clear(x{i});
+            end
         end
-    end
-end
-clear x identifier2 y str1 str2 i j ans;
+    clear x identifier2 y str1 str2 j i ans col f file filename appfiles k ref ref1 row l m u;
+    e = 0;
+    case 5
+        y = 0;
+        x = who();
+        identifier2 = {'i', 'identifier2','x','str1','str2','j','i', 'CSV','ASC', 'fold_list'};
+        for i=1:length(x)
+            for j=1:length(identifier2)
+                str1 = char(x{i});
+                str2 = char(identifier2{j});
+                y = y + strncmpi(str1, str2, 3);
+            end
+            if y == 0
+                clear(x{i});
+            end
+        end
+    clear x identifier2 y str1 str2 j i ans col f file filename appfiles k ref ref1 row l m u;
 e = 0;
     case 6
+        
+        appfiles1 = who(); appfiles = cell(0);
+for i = 1:length(appfiles1) %returns ASC files as appfiles 
+   x = strncmpi(appfiles1{i},'ASC',3);
+   if x ~= 1
+       appfiles{i} = '';
+   else
+       appfiles{end+1} = appfiles1{i}; %#ok<SAGROW>
+   end
+end
+for i = 1:length(appfiles)
+    x = strncmpi(appfiles{i},'',1);
+    if x == 1
+        appfiles = appfiles(1:i-1);
+        break
+    end
+end
+clear appfiles1;
+for i=1:length(appfiles) 
+    file = strcat(appfiles{i}, '.textdata');
+    evalc(['ref=' file]);
+    fprintf('File Successfully Imported (%d)\n', i);
+    ref = ref(93:end,1:end);
+    [row,~] = size(ref);
+    j = 1; col = 0; m = 0;
+    while col == 0 % find the first mention of MSG
+        j = j + 1;
+        k = char(ref{j,1});
+        col = strncmpi(k,'MSG',3);
+        if col == 1
+            m = m + 1;
+            col = 0;
+            if m == 3
+                break
+            end
+        end
+    end
+
+    ref1 = ref(1:j-1,1:end);
+    ref = ref(j:end,1:end);
+    filename = 'taringSCL_GSR'; 
+    l = 'ASC'; k = num2str(i);
+    f = strcat(l,k);
+    evalc([f '= setfield(' f ',filename,ref1)']);  % Save tare file to struct
+    for u=1:2
+    filename = ref{1,6};% rename filename
+    filename = strsplit(filename, '\hab_video'); filename = filename{2}; filename = strsplit(filename, '.'); filename = filename{1}; filename = strcat('GSRVideo',filename);
+    while col == 0 % find the next mention of MSG
+        j = j + 1;
+        k = char(ref{j,1});
+        col = strncmpi(k,'MSG',3);
+        [row,~] = size(ref);
+        if j == row
+            break
+        end
+    end
+    ref1 = ref(1:j-1,1:end);
+    ref = ref(j:end,1:end);
+    j = 1; col = 0;
+    evalc([f '= setfield(' f ',filename,ref1)']);
+    end
+    fprintf('File Successfully Processed (%d)\n', i)
+end
+        
+    case 7
         e = 1;
         clear choice;
 end
